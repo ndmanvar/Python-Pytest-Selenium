353a91647f310db0089185ff3b0da5c865326f47
Neil <neil@Neils-MacBook-Pro.local>
2015-08-18 13:33:15 -0700

    Using unittest so that getting id of test case is easy

--- test_functional.py
+++ test_functional.py
@@ -1,7 +1,32 @@
-def test_python_org(browser):
-    browser.get("http://www.google.com")
-    if not "Google" in browser.title:
-        raise Exception("Unable to load google page!")
-    elem = browser.find_element_by_name("q")
-    elem.send_keys("Sauce Labs")
-    elem.submit()
+import unittest
+import os
+import pytest
+from selenium import webdriver
+
+class GoogleTest(unittest.TestCase):
+
+    def setUp(self):
+        username = os.environ['SAUCE_USERNAME']
+        access_key = os.environ['SAUCE_ACCESS_KEY']
+
+        capabilities = {
+            'platform': "XP",
+            'browserName': "chrome",
+            'version': "31",
+            'name': self.id()
+        }
+        self.driver = webdriver.Remote(
+            command_executor = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (username, access_key),
+            desired_capabilities = capabilities)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_google(self):
+        driver = self.driver
+        driver.get("http://www.google.com")
+        if not "Google" in driver.title:
+            raise Exception("Unable to load google page!")
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("Sauce Labs")
+        elem.submit()
